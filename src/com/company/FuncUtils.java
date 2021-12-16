e57e883870c6ac07046ca66bd33c551be2016896
Dmitrii Sviridov <dm.sviridov99@yandex.ru>
2021-12-16 18:41:46 +0300

    Calculate method implements

--- src/com/company/FuncUtils.java
+++ src/com/company/FuncUtils.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Locale;
+import java.util.Vector;
 
 public class FuncUtils {
     static String[] functions = {"sin", "cos", "sqr", "sqrt", "pow"};
@@ -17,24 +18,54 @@ public class FuncUtils {
     {
         return (Arrays.asList(functions).contains(value.toLowerCase(Locale.ROOT)));
     }
+    public static boolean isOp(String value)
+    {
+        return (Arrays.asList(operators).contains(value.toLowerCase(Locale.ROOT)));
+    }
 
-    public static double ExecuteFunc(String func, double[] args) throws Exception {
-        if(!isFunc(func)){
-            throw new Exception("Invalid function");
-        }
-        switch (func.toLowerCase(Locale.ROOT)){
-            case ("sin"):
-                return Math.sin(Math.toRadians(args[0]));
-            case ("cos"):
-                return Math.cos(Math.toRadians(args[0]));
-            case ("sqr"):
-                return args[0]*args[0];
-            case ("sqrt"):
-                return Math.sqrt(args[0]);
-            case ("pow"):
-                return Math.pow(args[0], args[1]);
+    public static int getOperandsCount(String op){
+        switch (op){
+            case "sin":
+            case "cos":
+            case "sqr":
+            case "sqrt":
+                return 1;
+            case "pow":
+            case "+":
+            case "-":
+            case "/":
+            case "*":
+                return 2;
             default:
-                throw new Exception("Function " + func + " not implemented");
+                return 0;
+        }
+    }
+
+    public static double Execute(String op, Vector<Double> args) throws Exception {
+        if(!isFunc(op) && !isOp(op)){
+            throw new Exception("Invalid token");
+        }
+        switch (op.toLowerCase(Locale.ROOT)){
+            case ("sin"):
+                return Math.sin(Math.toRadians(args.get(0)));
+            case ("cos"):
+                return Math.cos(Math.toRadians(args.get(0)));
+            case ("sqr"):
+                return args.get(0)*args.get(0);
+            case ("sqrt"):
+                return Math.sqrt(args.get(0));
+            case ("pow"):
+                return Math.pow(args.get(0), args.get(1));
+            case "+":
+                return args.get(0)+args.get(1);
+            case "-":
+                return args.get(0)-args.get(1);
+            case "/":
+                return args.get(0)/args.get(1);
+            case "*":
+                return args.get(0)*args.get(1);
+            default:
+                throw new Exception(op + " not implemented");
         }
     }
 }
--- src/com/company/PostfixCalculator.java
+++ src/com/company/PostfixCalculator.java
@@ -1,10 +1,28 @@
 package com.company;
 
+import java.util.Stack;
 import java.util.Vector;
 
 public class PostfixCalculator {
-    public static double Calculate(Vector<Token> postfixExp){
-        double res = 0;
-        return res;
+    public static double Calculate(Vector<Token> postfixExp) throws Exception {
+        Stack<Double> stack = new Stack<Double>();
+        for(Token token : postfixExp) {
+            if (token.getType() == TokenTypes.NUM)
+                stack.push(Double.parseDouble(token.getValue()));
+            else if (token.getType() == TokenTypes.OP || token.getType() == TokenTypes.FUNC) {
+                Vector<Double> argsVector = new Vector<Double>(FuncUtils.getOperandsCount(token.getValue()));
+                try {
+                    for (int i = 0; i < FuncUtils.getOperandsCount(token.getValue()); i++)
+                        argsVector.add(stack.pop());
+                } catch (Exception e) {
+                    throw new Exception("Invalid expression");
+                }
+                stack.push(FuncUtils.Execute(token.getValue(), argsVector));
+            }
+        }
+        if(stack.size()==1)
+            return stack.pop();
+        else
+            throw new Exception("Invalid expression");
     }
 }
